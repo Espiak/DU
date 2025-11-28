329431b63d6cc3b609fbe5541de495e84c5db383
Hugo Hogh(laptop) <hoghhugo@gmail.com>
2025-11-28 21:51:25 +0100

    uloha 10 zufalstvo

--- uloha10.c
+++ uloha10.c
@@ -1,22 +1,27 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 int main(int argc, char *argv[])
 {
-    
+
+    int x = 0;
+
     // Inicializace SDL
     SDL_Init(SDL_INIT_VIDEO);
 
     // Vytvoření okna
     SDL_Window *window = SDL_CreateWindow("Vločky", 700, 300, 500, 500, SDL_WINDOW_SHOWN);
 
-    
     // Vytvoření kreslítka
-    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     SDL_Event event;
     int running = 1;
     int line_x = 100;
 
-    SDL_Texture* image = IMG_LoadTexture(renderer, "snowflake.png");
+    SDL_Surface *image = SDL_LoadBMP("snowflake.bmp");
+    // SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+    // SDL_FreeSurface(image);
+
 
     while (running == 1)
     {
@@ -29,12 +34,20 @@ int main(int argc, char *argv[])
                 running = 0;
             }
         }
+if (event.type == SDL_MOUSEBUTTONUP || event.button.button == SDL_BUTTON_LEFT)
+{   printf("Event type: %d\n", event.type);
 
+    printf("pressed\n");
+    x += 100;
+}
+
+
+        //printf(" %d ",SDL_MOUSEBUTTONUP);
         // Posuň pozici čáry doprava
         line_x++;
 
         // backgrund collour
-       SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 0, x, x, 255);
 
         // every frame gets new background
         SDL_RenderClear(renderer);

3f3f465c1d65719b6b5a321e1f6654dce8dabfe9
Hugo Hogh(laptop) <hoghhugo@gmail.com>
2025-12-18 21:09:56 +0100

    zacinam projekt

--- uloha10.c
+++ uloha10.c
@@ -1,60 +1,70 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+void alocate(int clicks){
+    int p = relloc(sizeof(SDL_Rect) * clicks);
+}
+
 int main(int argc, char *argv[])
 {
-
+    int running = 1;
+    int rotacia = 100;
     int x = 0;
 
     // Inicializace SDL
     SDL_Init(SDL_INIT_VIDEO);
 
-    // Vytvoření okna
     SDL_Window *window = SDL_CreateWindow("Vločky", 700, 300, 500, 500, SDL_WINDOW_SHOWN);
-
-    // Vytvoření kreslítka
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
     SDL_Event event;
-    int running = 1;
-    int line_x = 100;
+    SDL_Texture *paint = IMG_LoadTexture(renderer, "snowflake.png");
 
-    SDL_Surface *image = SDL_LoadBMP("snowflake.bmp");
-    // SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
-    // SDL_FreeSurface(image);
+    
+    
+    SDL_Rect* Position[5];
+    
+    SDL_Rect* Position[2] = { 50, 50, 50, 50 };
+    Position[2]->w = 20;
+    
+    int clicks = 0;
+
+    if (paint == NULL)
+    {
+        printf("bez obrazku");
+        return 1;
+    }
 
     while (running == 1)
     {
-        // Dokud jsou k dispozici nějaké události, ukládej je do proměnné `event`
-        while (SDL_PollEvent(&event))
+        
+        while (SDL_PollEvent(&event))// Dokud jsou k dispozici nějaké události, ukládej je do proměnné `event`
         {
-            // Pokud došlo k uzavření okna, nastav proměnnou `running` na `0`
-            if (event.type == SDL_QUIT)
+            
+            if (event.type == SDL_QUIT)// Pokud došlo k uzavření okna, nastav proměnnou `running` na `0`
             {
                 running = 0;
             }
             if (event.type == SDL_MOUSEBUTTONDOWN)
             {
-
-                x += 10;
+                SDL_GetMouseState(&position[clicks].x, &position[clicks].y);
+                clicks += 1;
                 printf("Event type: %d\n", event.type);
+                //alocate(clicks);
             }
         }
 
-        // printf(" %d ",SDL_MOUSEBUTTONUP);
-        //  Posuň pozici čáry doprava
-        line_x++;
+        rotacia++;
+        for(int k = 0;k<5 ;k++ )
+        {
+            position[k].y++;
+        }
 
-        // backgrund collour
-        SDL_SetRenderDrawColor(renderer, 0, x, x, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);        // backgrund collour
+        SDL_RenderClear(renderer);                             // every frame gets new background
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);  // colour of item
 
-        // every frame gets new background
-        SDL_RenderClear(renderer);
 
-        // colour of item
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-
-        // Vykresli čáru
-        SDL_RenderDrawLine(renderer, line_x, 100, line_x, 500);
+        SDL_RenderCopyEx(renderer, paint, NULL, &position, rotacia, NULL, x);
 
         // Zobraz vykreslené prvky na obrazovku
         SDL_RenderPresent(renderer);
@@ -63,6 +73,10 @@ int main(int argc, char *argv[])
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    for (int i = 0; i < 3; i++) {
+    free(Position[i]);
+}
+
 
     return 0;
 }
